1c6278ee74d44823fbc3802fefec03a99ff9f002
sumpen99 <sumpen841001@hotmail.com>
2024-02-20 21:11:52 +0100

    first init

---
+++ WeraTents/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeraTents
+//
+//  Created by fredrik sundström on 2024-02-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ WeraTents/WeraTentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeraTentsApp.swift
+//  WeraTents
+//
+//  Created by fredrik sundström on 2024-02-20.
+//
+
+import SwiftUI
+
+@main
+struct WeraTentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
