7d35df10114f6bb4be329b323468d7e3e74ca1fd
sumpen99 <sumpen841001@hotmail.com>
2024-03-27 17:32:05 +0100

    removed local fetching

--- WeraTents/Global/Variables.swift
+++ WeraTents/Global/Variables.swift
@@ -5,11 +5,8 @@
 //  Created by fredrik sundström on 2024-02-23.
 //
 
-import SwiftUI
 
-let FETCH_LOCALLY:Bool              = false
 let BULLET                          = "•\t"
-let MAX_STORAGE_PNG_SIZE:Int64      = 1 * 1024 * 1024
 let CORNER_RADIUS_CAROUSEL          = 18.0
 let CORNER_RADIUS_BRAND             = 5.0
 let MENU_HEIGHT_HEADER              = 40.0
@@ -18,4 +15,4 @@ let CAPTURED_HEIGHT_STORE           = 850.0
 let HOME_BRAND_HEIGHT               = 75.0
 let V_SPACING_REG                   = 10.0
 let V_GRID_SPACING                  = 20.0
-let AR_SELECTED_IMAGE:CGFloat       = 100.0
+let AR_SELECTED_IMAGE               = 100.0
